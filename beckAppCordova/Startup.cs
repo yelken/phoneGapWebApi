cd3a527476330ed862960be3b0d37503b79123cf
renatocantarino <renato.cantarino@gmail.com>
2015-03-05 20:30:00 -0300

    initial

---
+++ beckAppCordova/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(beckAppCordova.Startup))]
+namespace beckAppCordova
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
